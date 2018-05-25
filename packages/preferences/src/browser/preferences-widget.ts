d0e605a3a19d9308af15fcad0e5d07dcb55d9502
Igor Vinokur <ivinokur@redhat.com>
2018-05-25 16:35:55 +0300

    commit

--- packages/preferences/src/browser/preferences-widget.ts
+++ packages/preferences/src/browser/preferences-widget.ts
@@ -5,16 +5,15 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import {inject, multiInject} from "inversify";
+import { inject, multiInject } from "inversify";
 import { VirtualWidget } from "@theia/core/lib/browser/widgets/virtual-widget";
 import { Message } from "@phosphor/messaging";
 import { EditorPreferences } from "@theia/editor/lib/browser/editor-preferences";
 import { PreferenceService } from "../../../core/lib/browser/preferences";
-import { GitPreferences } from "@theia/git/lib/browser/git-preferences";
 import { EditorManager } from "@theia/editor/lib/browser";
 import { ApplicationShell, PreferenceSchema, PreferenceScope } from "@theia/core/lib/browser";
 import { PreferenceProperty } from "@theia/core/lib/browser/preferences/preference-contribution";
-import { h, VirtualElement } from '@phosphor/virtualdom';
+import { h } from '@phosphor/virtualdom';
 
 export interface PreferenceGroup {
     name: string;
@@ -31,10 +30,8 @@ export class PreferencesWidget extends VirtualWidget {
 
     scope: PreferenceScope;
     protected preferencesGroups: PreferenceGroup[];
-    protected oldPreferenceName: string;
 
     constructor(@inject(EditorPreferences) protected readonly editorPreferences: EditorPreferences,
-                @inject(GitPreferences) protected readonly gitPreferences: GitPreferences,
                 @inject(EditorManager) protected readonly editorManager: EditorManager,
                 @multiInject(PreferenceSchema) protected readonly preferenceSchema: PreferenceSchema[],
                 @inject(ApplicationShell) protected readonly applicationShell: ApplicationShell,
@@ -62,6 +59,7 @@ export class PreferencesWidget extends VirtualWidget {
                 isExpanded: false
             });
         }
+
         this.update();
     }
 
@@ -91,97 +89,70 @@ export class PreferencesWidget extends VirtualWidget {
     }
 
     protected renderPreferenceGroup(group: PreferenceGroup): h.Child {
-        return h.div({className: "preference-group"},
+        return h.div(
+            {className: "preference-group"},
             h.div(
-                {className: "toggle",
+                {
+                    className: "preference-group-header-div",
                     onclick: () => {
                         group.isExpanded = !group.isExpanded;
                         this.update();
                     }
                 },
-                h.div({className: "number"}, group.name),
+                group.name,
                 h.div({className: group.isExpanded ? "icon fa fa-caret-down" : "icon fa fa-caret-right"})
             ),
-            group.isExpanded ? this.renderPreferencesList(group.preferences) : ""
+            group.isExpanded ? this.createPreferencesList(group.preferences) : ""
         );
     }
 
-    protected renderPreferencesList(preferences: Preference[]): h.Child {
-        const files: h.Child[] = [];
-        for (const preference of preferences) {
-            files.push(this.renderPreferenceItem(preference));
-        }
-        const commitFiles = h.div({className: "preferences-list"}, ...files);
-        return h.div({className: "preference-body"}, commitFiles);
+    protected createPreferencesList(preferences: Preference[]): h.Child {
+        const preferenceItems: h.Child[] = [];
+        preferences.forEach( preference => {
+            preferenceItems.push(this.createPreferenceItem(preference));
+        });
+        return h.div({className: "preferences-list"}, ...preferenceItems);
     }
 
-    protected renderPreferenceItem(preference: Preference): h.Child {
+    protected createPreferenceItem(preference: Preference): h.Child {
         const preferenceName: string = preference.name;
-        const nameSpan = h.span({
-            className: this.preferenceService.get(preferenceName) ? 'preference-item-name-saved' : 'preference-item-name-unsaved',
-            title: preference.property.description
-        }, preferenceName);
+        const nameSpan = h.span(
+            {
+                className: this.preferenceService.get(preferenceName) ? 'preference-item-name-saved' : 'preference-item-name-unsaved',
+                title: preference.property.description
+            },
+            preferenceName);
         const property = preference.property;
-        const enumItems: h.Child[] = [];
-        let valueContainer;
-        if (property.type === 'boolean') {
-            enumItems.push(this.createEnumItem(preferenceName, 'true'));
-            enumItems.push(this.createEnumItem(preferenceName, 'false'));
-        } else if (property.enum) {
-            property.enum.forEach(item => {
-                enumItems.push(this.createEnumItem(preferenceName, item));
-            });
-        }
-        if (enumItems.length !== 0) {
-            valueContainer = h.div({className: 'preference-item-value-select-container-span'}, ...enumItems);
+        const onBlur = function(event: FocusEvent): void  {
+            const relatedTarget: any = event.relatedTarget;
+            if (relatedTarget && relatedTarget.className.startsWith('preference-item-value-')) {
+                return;
+            }
+            PreferencesWidget.hideElement(preferenceName);
+        };
+        let valueContainer: h.Child;
+        if (property.type === 'boolean' || property.enum) {
+            valueContainer = this.createEnumItems(preferenceName, property);
         } else {
-            const defaultValue = property.default ? property.default : "";
-            const buttonAdd = h.button({
-                className: 'preference-item-value-input-button',
-                id: preferenceName + '-value-input-button',
-                onclick: () => {
-                    const inputValue: any = document.getElementById('value-input-' + preferenceName);
-                    if (inputValue) {
-                        const value: string = inputValue.value;
-                        this.handleElement(preferenceName, value.length === 0 ? defaultValue : value);
-                    }
-                }
-            });
-            const onBlur = function(event: FocusEvent): void  {
-                const target: any = event.relatedTarget;
-                if (target && target.className.startsWith('preference-item-value-')) {
-                    return;
-                }
-                PreferencesWidget.hideValue(preferenceName);
-            };
-            const inputElement = h.input({
-                className: 'preference-item-value-input-input',
-                placeholder: defaultValue,
-                type: property.type ? property.type.toString() : "string",
-                id: 'value-input-' + preferenceName,
-                onblur: onBlur
-            });
-            valueContainer = h.div({
-                className: 'preference-item-value-input-div',
-                id: 'value-input-id-' + preferenceName,
-                tabindex: '0',
-                onblur: onBlur
-            }, inputElement, buttonAdd);
+            valueContainer = this.createInputValuePanel(preferenceName, property, onBlur);
         }
-        const editDiv = h.div({className: "preference-item-edit-div"},
-            h.div({
+        const editDiv = this.createEditDiv(preferenceName, property, onBlur, valueContainer);
+        return h.div({className: 'preference-item-div'}, editDiv, nameSpan);
+    }
+
+    protected createEditDiv(preferenceName: string, property: PreferenceProperty, onBlur: (event: FocusEvent) => void, valueContainer: h.Child): h.Child {
+        return h.div(
+            {
+                className: "preference-item-edit-div"
+            },
+            h.div(
+                {
                     className: 'preference-item-edit-icon-div',
                     id: 'pencil-icon-container-' + preferenceName,
                     tabindex: '0',
+                    onblur: onBlur,
                     onclick: () => {
-                        if (this.oldPreferenceName) {
-                            PreferencesWidget.hideValue(this.oldPreferenceName);
-                        }
-                        this.oldPreferenceName = preferenceName;
-                        const button = document.getElementById(preferenceName + '-value-input-button');
-                        if (button) {
-                            button.innerHTML = this.preferenceService.get(preferenceName) ? 'Edit' : 'Add Value';
-                        }
+                        this.update();
                         const valueDiv = document.getElementById('value-container-' + preferenceName);
                         if (valueDiv) {
                             valueDiv.style.display = "block";
@@ -192,36 +163,88 @@ export class PreferencesWidget extends VirtualWidget {
                         }
                     }
                 },
-                h.i({
-                    className: "icon fa fa-pencil",
-                    title: this.preferenceService.get(preferenceName) ? 'Edit' : 'Add Value'
-                })
+                h.i(
+                    {
+                        className: "icon fa fa-pencil",
+                        title: this.preferenceService.get(preferenceName) ? 'Edit' : 'Add Value'
+                    }
+                )
             ),
-            h.div({
-                className: 'preference-item-edit-container-div',
-                id: 'value-container-' + preferenceName,
-            }, valueContainer)
+            h.div(
+                {
+                    className: 'preference-item-edit-container-div',
+                    id: 'value-container-' + preferenceName,
+                },
+                valueContainer)
         );
-        return h.div({className: 'preference-item-div'}, editDiv, nameSpan);
     }
 
-    protected createEnumItem(preferenceName: string, value: string): VirtualElement {
-        return h.span({
+    protected createEnumItems(preferenceName: string, property: PreferenceProperty): h.Child {
+        const enumItems: h.Child[] = [];
+        if (property.type === 'boolean') {
+            enumItems.push(this.createEnumItem(preferenceName, 'true'));
+            enumItems.push(this.createEnumItem(preferenceName, 'false'));
+        } else if (property.enum) {
+            property.enum.forEach(item => {
+                enumItems.push(this.createEnumItem(preferenceName, item));
+            });
+        }
+        return h.div({className: 'preference-item-value-select-container-span'}, ...enumItems);
+    }
+
+    protected createEnumItem(preferenceName: string, value: string): h.Child {
+        return h.span(
+            {
                 className: 'preference-item-value-select-span',
+                tabindex: '0',
                 onclick: () => {
-                    this.handleElement(preferenceName, value);
+                    this.preferenceService.set(preferenceName, value, this.scope);
+                    PreferencesWidget.hideElement(preferenceName);
                 }
             },
             value
         );
     }
 
-    protected handleElement(preferenceName: string, value: string): void {
-        this.preferenceService.set(preferenceName, value, this.scope);
-        PreferencesWidget.hideValue(preferenceName);
+    protected createInputValuePanel(preferenceName: string, property: PreferenceProperty, onBlur: (event: FocusEvent) => void): h.Child {
+        const defaultValue = property.default ? property.default : "";
+        const buttonElement = h.button(
+            {
+                className: 'preference-item-value-input-button',
+                id: preferenceName + '-value-input-button',
+                onclick: () => {
+                    const inputValue: any = document.getElementById('value-input-' + preferenceName);
+                    if (inputValue) {
+                        const value: string = inputValue.value;
+                        this.preferenceService.set(preferenceName, value.length === 0 ? defaultValue : value, this.scope);
+                        PreferencesWidget.hideElement(preferenceName);
+                    }
+                }
+            },
+            this.preferenceService.get(preferenceName) ? 'Edit' : 'Add Value'
+        );
+        const inputElement = h.input(
+            {
+                className: 'preference-item-value-input-input',
+                placeholder: defaultValue,
+                type: property.type ? property.type.toString() : "string",
+                id: 'value-input-' + preferenceName,
+                onblur: onBlur
+            }
+        );
+        return h.div(
+            {
+                className: 'preference-item-value-input-div',
+                id: 'value-input-id-' + preferenceName,
+                tabindex: '0',
+                onblur: onBlur
+            },
+            inputElement,
+            buttonElement
+        );
     }
 
-    private static hideValue(preferenceName: string): void {
+    private static hideElement(preferenceName: string): void {
         const valueDiv = document.getElementById('value-container-' + preferenceName);
         if (valueDiv) {
             valueDiv.style.display = 'none';
